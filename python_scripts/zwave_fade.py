eb9e529deefc2637c8fcd128494a6763babdc0fe
Joshua <c.treyce@gmail.com>
2018-07-21 17:34:56 -0600

    mqtt, computer control, zwave fade
    - Add MQTT
    - Add computer control package
    - Add zwave fade script
      - It's a bit janky, it's not smooth and the script runs slow because zwave
    - Tiles! Add configuration tiles custom ui to add buttons to enable pihole and purge sql db

---
+++ python_scripts/zwave_fade.py
@@ -0,0 +1,76 @@
+"""
+https://community.home-assistant.io/t/light-fade-in/35509
+
+automation:
+- alias: Light 15min Fade to 100% starting an hour before sunset
+    trigger:
+    platform: sun
+    event: sunset
+    offset: "-01:00:00"
+    action:
+    - service: python_script.zwave_fade
+        data:
+          entity_id: light.level  # Entity ID to fade
+          transition: 20  # Seconds to go from current to end brightness
+          brightness: 255  # Brightness to end at
+          brightness_pct: 100  # Brightness to end at as a percentage
+"""
+
+entity_id = data.get('entity_id')
+brightness = data.get('brightness', None)
+brightness_pct = data.get('brightness_pct', None)
+
+if entity_id is not None and (brightness is not None or brightness_pct is not None):
+    light = hass.states.get(entity_id)
+
+    start_level = light.attributes.get('brightness', 0)
+    transition = data.get('transition', 0)
+
+    """ Use brightness or convert brightness_pct """
+    end_level = int(brightness) if brightness is not None else math.ceil(
+        brightness_pct * 2.55)
+
+    """ Calculate number of steps """
+    steps = int(math.fabs((start_level - end_level)))
+    fadeout = True if start_level > end_level else False
+
+    """ Calculate the delay time """
+    delay = round(transition / steps, 3)
+
+    """ Disable delay anbd increase stepping if delay < 3/4 second """
+    if (delay < .750):
+        delay = 0
+        steps = int(steps / 5)
+        step_by = 5
+    else:
+        step_by = 1
+
+    logger.info('Setting brightness of ' + str(entity_id) +
+                ' from ' + str(start_level) + ' to ' + str(end_level) +
+                ' steps ' + str(steps) + ' delay ' + str(delay))
+
+    new_level = start_level
+    for x in range(steps):
+        current_level = light.attributes.get('brightness', 0)
+        if (fadeout and current_level < new_level):
+            break
+        elif (not fadeout and current_level > new_level):
+            break
+        else:
+            data = {"entity_id": entity_id, "brightness": new_level}
+            hass.services.call('light', 'turn_on', data)
+            if (fadeout):
+                new_level = new_level - step_by
+            else:
+                new_level = new_level + step_by
+            """ Do not sleep for 0 delay """
+            if (delay > 0):
+                time.sleep(delay)
+
+""" Ensure light ends at the final state """
+if (end_level > 0):
+    data = {"entity_id": entity_id, "brightness": end_level}
+    hass.services.call('light', 'turn_on', data)
+else:
+    data = {"entity_id": entity_id}
+    hass.services.call('light', 'turn_off', data)
