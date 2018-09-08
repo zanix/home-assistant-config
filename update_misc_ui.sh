25cb3de2b1881098e2f2a142c7491bb43056c917
Joshua <c.treyce@gmail.com>
2018-09-07 21:35:31 -0600

    upgrade v0.77.3, notification changes
    - Upgrade to v0.77.3
    - Update notifications
    - Update custom_ui
    - Remove custom-tiles from update_misc_ui since the repo was removed

--- update_misc_ui.sh
+++ update_misc_ui.sh
@@ -67,5 +67,5 @@ get_file state-card-hline.html https://raw.githubusercontent.com/covrig/homeassi
 get_file state-card-hline_es5.html https://raw.githubusercontent.com/covrig/homeassistant-hline/master/www/custom_ui/state-card-hline_es5.html www/custom_ui/
 
 # Tiles
-get_file state-card-tiles.html https://raw.githubusercontent.com/c727/home-assistant-tiles/master/www/custom_ui/state-card-tiles.html www/custom_ui/
-get_file state-card-tiles_es5.html https://raw.githubusercontent.com/c727/home-assistant-tiles/master/www/custom_ui/state-card-tiles_es5.html www/custom_ui/
+# get_file state-card-tiles.html https://raw.githubusercontent.com/c727/home-assistant-tiles/master/www/custom_ui/state-card-tiles.html www/custom_ui/
+# get_file state-card-tiles_es5.html https://raw.githubusercontent.com/c727/home-assistant-tiles/master/www/custom_ui/state-card-tiles_es5.html www/custom_ui/
