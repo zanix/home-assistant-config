caf34104d40dd6c9aced85956eb8263ded738c89
Joshua Clark <zanix@users.noreply.github.com>
2021-03-28 22:52:05 +0000

    revert unifigateway component

--- custom_components/unifigateway/sensor.py
+++ custom_components/unifigateway/sensor.py
@@ -18,18 +18,16 @@ from homeassistant.const import (
     CONF_NAME, CONF_HOST, CONF_USERNAME, CONF_PASSWORD,
     CONF_MONITORED_CONDITIONS, CONF_VERIFY_SSL)
 
-__version__ = '0.3.2'
+__version__ = '0.2.3'
 
 _LOGGER = logging.getLogger(__name__)
 
 CONF_PORT = 'port'
 CONF_SITE_ID = 'site_id'
-CONF_UNIFI_VERSION = 'version'
 
 DEFAULT_NAME = 'UniFi Gateway'
 DEFAULT_HOST = 'localhost'
-DEFAULT_PORT = 443
-DEFAULT_UNIFI_VERSION = 'v5'
+DEFAULT_PORT = 8443
 DEFAULT_SITE = 'default'
 DEFAULT_VERIFY_SSL = False
 
@@ -53,7 +51,7 @@ USG_SENSORS = {
     SENSOR_FIRMWARE:['Firmware Upgradable', '', 'mdi:database-plus']
 }
 
-POSSIBLE_MONITORED = [ SENSOR_VPN, SENSOR_WWW, SENSOR_WAN, SENSOR_LAN,
+POSSIBLE_MONITORED = [ SENSOR_VPN, SENSOR_WWW, SENSOR_WAN, SENSOR_LAN, 
                         SENSOR_WLAN, SENSOR_ALERTS, SENSOR_FIRMWARE ]
 DEFAULT_MONITORED = POSSIBLE_MONITORED
 
@@ -63,9 +61,8 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_SITE_ID, default=DEFAULT_SITE): cv.string,
     vol.Required(CONF_PASSWORD): cv.string,
     vol.Required(CONF_USERNAME): cv.string,
-    vol.Optional(CONF_UNIFI_VERSION, default=DEFAULT_UNIFI_VERSION): cv.string,
-    vol.Optional(CONF_PORT, default=DEFAULT_PORT): cv.port,
-    vol.Optional(CONF_VERIFY_SSL, default=DEFAULT_VERIFY_SSL):
+    vol.Required(CONF_PORT, default=DEFAULT_PORT): cv.port,
+    vol.Optional(CONF_VERIFY_SSL, default=DEFAULT_VERIFY_SSL): 
         vol.Any(cv.boolean, cv.isfile),
     vol.Optional(CONF_MONITORED_CONDITIONS, default=DEFAULT_MONITORED):
         vol.All(cv.ensure_list, [vol.In(POSSIBLE_MONITORED)])
@@ -80,12 +77,11 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     username = config.get(CONF_USERNAME)
     password = config.get(CONF_PASSWORD)
     site_id = config.get(CONF_SITE_ID)
-    version = config.get(CONF_UNIFI_VERSION)
     port = config.get(CONF_PORT)
     verify_ssl = config.get(CONF_VERIFY_SSL)
 
     try:
-        ctrl = Controller(host, username, password, port, version,
+        ctrl = Controller(host, username, password, port, version='v4',
                           site_id=site_id, ssl_verify=verify_ssl)
     except APIError as ex:
         _LOGGER.error("Failed to connect to Unifi Security Gateway: %s", ex)
@@ -134,34 +130,32 @@ class UnifiGatewaySensor(Entity):
         from pyunifi.controller import APIError
 
         if self._sensor == SENSOR_ALERTS:
-          self._attributes = {}
-
           try:
               unarchived_alerts = self._ctrl.get_alerts()
           except APIError as ex:
               _LOGGER.error("Failed to access alerts info: %s", ex)
-          else:
-              for index, alert in enumerate(unarchived_alerts,start=1):
-                  if not alert['archived']:
-                      self._attributes[str(index)] = alert
 
-              self._state = len(self._attributes)
+          self._attributes = {}
+          for index, alert in enumerate(unarchived_alerts,start=1):
+            if not alert['archived']:
+              self._attributes[str(index)] = alert
+
+          self._state = len(self._attributes)
 
         elif self._sensor == SENSOR_FIRMWARE:
-          self._attributes = {}
-          self._state = 0
-
           try:
             aps = self._ctrl.get_aps()
           except APIError as ex:
             _LOGGER.error("Failed to scan aps: %s", ex)
-          else:
-            # Set the attributes based on device name - this may not be unique
-            # but is user-readability preferred
-            for devices in aps:
-              if devices.get('upgradable'):
-                  self._attributes[devices['name']] = devices['upgradable']
-                  self._state += 1
+
+          # Set the attributes based on device name - this may not be unique
+          # but is user-readability preferred
+          self._attributes = {}
+          self._state = 0
+          for devices in aps:
+            if devices.get('upgradable'):
+                self._attributes[devices['name']] = devices['upgradable']
+                self._state += 1
 
         else:
           # get_healthinfo() call made for each of 4 sensors - should only be for 1
