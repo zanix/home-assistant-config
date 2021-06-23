5a0151c70a9e87f6b50b7f81822a10f5eed03db9
Joshua Clark <zanix@users.noreply.github.com>
2021-06-23 03:49:31 +0000

    update hass v2021.6.6, hacs, components - Update hass - Update hacs - Update keymaster, uptime card - Fix automation ids - Remove useless ring sensor templates

--- custom_components/keymaster/const.py
+++ custom_components/keymaster/const.py
@@ -2,7 +2,7 @@
 from homeassistant.const import STATE_LOCKED, STATE_UNLOCKED
 
 DOMAIN = "keymaster"
-VERSION = "v0.0.47"
+VERSION = "v0.0.51"
 ISSUE_URL = "https://github.com/FutureTense/keymaster"
 PLATFORMS = ["binary_sensor", "sensor"]
 ZWAVE_NETWORK = "zwave_network"
