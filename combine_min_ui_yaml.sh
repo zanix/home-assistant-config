b64d00b7cfe09b561946523aca3c9f2c4e62ba14
Joshua Clark <zanix@users.noreply.github.com>
2022-05-18 04:34:01 +0000

    minimalist ui - Add in Minimalist UI default files   - I don't want them, but they keep being added anyway... - Add script to combine Minimalist UI yaml for copying into UI config - Add custom Minimalist UI cards - Add Mushroom colors for Minimalist UI

---
+++ combine_min_ui_yaml.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Combine yaml from Minimalist UI for use in UI config.
+#
+
+SOURCE=custom_components/ui_lovelace_minimalist/__ui_minimalist__/ulm_templates/
+SOURCE2=ui_lovelace_minimalist/custom_cards/colors/
+DEST=lovelace/minimalist.yaml
+
+# Cat all ui_minimalist yaml files into one.
+find $SOURCE -type f -name '*.yaml' -exec cat {} + > $DEST
+# Remove triple dashes.
+gawk -i inplace '!/---/' $DEST
+# Indent all line by 2 spaces.
+sed -i -e 's/^/  /' $DEST
+# Insert button_card_templates at the top.
+sed -i '1s/^/button_card_templates:\n/' $DEST
